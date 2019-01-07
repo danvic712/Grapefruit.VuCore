234b1af3f3d8302382af06f3fb7822babe37fb03
Lanesra712 <danvic96@hotmail.com>
2019-01-07 21:50:12 +0800

    Add generic class and generic entity operate interface

---
+++ 01_Entity/Grapefruit.Entity/BaseEntity.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file= "BaseEntity.cs">
+//     Copyright (c) Danvic712. All rights reserved.
+// </copyright>
+// Author: Danvic712
+// Created DateTime: 2019/1/7 20:05:46 
+// Modified by:
+// Description: 泛型实体基类
+//-----------------------------------------------------------------------
+using System;
+
+namespace Grapefruit.Entity
+{
+    /// <summary>
+    /// 泛型实体基类
+    /// </summary>
+    /// <typeparam name="TPrimaryKey">主键</typeparam>
+    public abstract class BaseEntity<TPrimaryKey>
+    {
+        #region Attributes
+
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public virtual TPrimaryKey Id { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public virtual TPrimaryKey Creator { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public virtual DateTime CreateTime { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// 最后修改者
+        /// </summary>
+        public virtual TPrimaryKey Editor { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public virtual DateTime EditTime { get; set; }
+
+        #endregion
+    }
+
+    #region Implements
+
+    /// <summary>
+    /// Guid 类型主键实体基类
+    /// </summary>
+    public abstract class BaseEntity : BaseEntity<Guid>
+    { }
+
+    #endregion
+}
---
+++ 03_Domain/Grapefruit.Domain/IRepository.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file= "IRepository.cs">
+//     Copyright (c) Danvic712. All rights reserved.
+// </copyright>
+// Author: Danvic712
+// Created DateTime: 2019/1/7 19:45:45 
+// Modified by:
+// Description: 泛型仓储接口
+//-----------------------------------------------------------------------
+using Grapefruit.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grapefruit.Domain
+{
+    /// <summary>
+    /// 泛型仓储接口定义
+    /// </summary>
+    public interface IRepository
+    { }
+
+    /// <summary>
+    /// 泛型仓储接口定义
+    /// </summary>
+    /// <typeparam name="TEntity">实体</typeparam>
+    /// <typeparam name="TPrimaryKey">主键</typeparam>
+    public interface IRepository<TEntity, TPrimaryKey> : IRepository where TEntity : BaseEntity<TPrimaryKey>
+    {
+        #region APIs
+
+        /// <summary>
+        /// 根据主键获取实体信息
+        /// </summary>
+        /// <param name="id">主键</param>
+        /// <returns></returns>
+        TEntity GetEntityById(TPrimaryKey id);
+
+        #endregion
+    }
+
+    #region Implements
+
+    /// <summary>
+    /// Guid 主键泛型仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IRepository<TEntity> : IRepository<TEntity, Guid> where TEntity : BaseEntity
+    { }
+
+    #endregion
+}
